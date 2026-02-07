47048f61380b891034a08dcef05273b655d07500
zahra <z.godarzi75@gmail.com>
2026-02-08 01:15:14 +0330

    feat:add validation to add user

--- src/components/user/add-user.tsx
+++ src/components/user/add-user.tsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { userSchema } from "../../constants/userSchemaValidation";
 import useCreateUser from "../../hooks/useCreateUser";
 import type { propsType } from "../../types/propstype";
 
 export default function AddUser({ onClose }: propsType) {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
+
+	const [nameError, setNameError] = useState("");
+	const [emailError, setEmailError] = useState("");
+	const [touched, setTouched] = useState({ name: false, email: false });
+
 	const { mutate, isPending } = useCreateUser();
+	useEffect(() => {
+		const result = userSchema.safeParse({ name, email });
+		if (!result.success) {
+			const errors = result.error.flatten().fieldErrors;
+			setNameError(errors.name?.[0] || "");
+			setEmailError(errors.email?.[0] || "");
+		} else {
+			setNameError("");
+			setEmailError("");
+		}
+	}, [name, email]);
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
+		const result = userSchema.safeParse({ name, email });
+		if (!result.success) return;
 		if (name.trim() && email.trim()) {
 			mutate(
 				{ name, email },
@@ -39,17 +58,32 @@ export default function AddUser({ onClose }: propsType) {
 						type="text"
 						placeholder="Name"
 						value={name}
-						onChange={(e) => setName(e.target.value)}
-						className="border rounded px-3 py-2"
+						onChange={(e) => {
+							setName(e.target.value);
+							setTouched((prev) => ({ ...prev, name: true }));
+						}}
+						className={`border rounded px-3 py-2 ${
+							touched.name && nameError ? "border-red-500" : ""
+						}`}
 					/>
-
+					{nameError && touched.name && (
+						<p className="text-red-500 text-sm">{nameError}</p>
+					)}
 					<input
 						type="text"
 						placeholder="Email"
 						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						className="border rounded px-3 py-2"
+						onChange={(e) => {
+							setEmail(e.target.value);
+							setTouched((prev) => ({ ...prev, email: true }));
+						}}
+						className={`border rounded px-3 py-2 ${
+							touched.email && emailError ? "border-red-500" : ""
+						}`}
 					/>
+					{emailError && touched.email && (
+						<p className="text-red-500 text-sm">{emailError}</p>
+					)}
 					<button
 						type="submit"
 						className="bg-green-500 text-white py-2 rounded hover:bg-green-600 disabled:opacity-50"
